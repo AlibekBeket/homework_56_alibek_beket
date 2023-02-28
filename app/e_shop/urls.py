790384f4a86121386a117963829d29f3c5ba9c2b
Alibek Beket <alibek1beket@gmail.com>
2023-02-28 15:13:34 +0600

    добавил функцию редактирования товара(product_update_view) с самой страницой(шаблон страницы form применил) и стелизовал его

--- app/e_shop/urls.py
+++ app/e_shop/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', products_list_view, name='products_list'),
     path('product/', products_list_view, name='products_list'),
     path('product/<int:pk>/', product_detail_view, name='product_detail'),
-    path('product/add/', product_add_view, name='product_add')
+    path('product/add/', product_add_view, name='product_add'),
+    path('product/<int:pk>/update', product_update_view, name='product_update')
 ]
