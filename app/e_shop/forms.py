48d68148cba493377af55326e2743c2584566e4b
Alibek Beket <alibek1beket@gmail.com>
2023-02-28 15:59:10 +0600

    исправил названия страниц и его обозначения, изменил функцию формы(price теперь модельформы)

--- app/e_shop/forms.py
+++ app/e_shop/forms.py
@@ -5,17 +5,18 @@ from e_shop.models import Product
 
 
 class ProductForm(forms.ModelForm):
+    price = forms.DecimalField(required=True, decimal_places=2, max_digits=7,
+                               min_value=100)
     the_rest_of_the_goods = forms.IntegerField(required=True, label='The rest of the goods:', min_value=0)
 
     class Meta:
         model = Product
-        fields = ("product_name", "product_description", "product_image", "category", "price")
+        fields = ("product_name", "product_description", "product_image", "category")
         labels = {
             'product_name': 'Название продукта',
             'product_description': 'Описание',
             'product_image': 'Фото продукта',
             'category': 'Категория',
-            'price': 'Цена'
         }
 
     def clean_product_name(self):
@@ -26,7 +27,6 @@ class ProductForm(forms.ModelForm):
 
     def clean_product_description(self):
         product_description = self.cleaned_data.get('product_description')
-        if len(product_description) < 3:
-            raise ValidationError('Описание продукта не может состоять из 1 или 2 символов')
+        if len(product_description) < 3 and len(product_description) != 0:
+            raise ValidationError('Описание продукта не может состоять из 1 или 2 символов, но может быть пустым')
         return product_description
-
